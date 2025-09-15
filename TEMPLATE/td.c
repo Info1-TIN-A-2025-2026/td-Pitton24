fb27d69c67b1db01387acc99acd9f3bb8d2235ac
Pitton24 <samuel.pitton@heig-vd.ch>
2025-09-15 14:10:27 +0000

    MAJ

--- TEMPLATE/td.c
+++ TEMPLATE/td.c
@@ -2,6 +2,6 @@
 
 int main(int argc, const char *argv[])
 {
-	puts("Podemos trabalhar...!");
+	puts("ahahahahahahahaahahaha je veux du fromage et du vin rouge et du pain aussi et du saucisson et du chocolat et du champagne");
 	return 0;
 }
