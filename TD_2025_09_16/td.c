fe7f8c9d63ce28adc6f1f6a04cad09d7c1e65ad4
Pitton24 <samuel.pitton@heig-vd.ch>
2025-09-16 12:33:08 +0000

    declaration des type

---
+++ TD_2025_09_16/td.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23; 
+	int account_value = -42; // signed int account_value = -42; // signed c'est optionnel
+
+	float temperature = 18.1; // f pour float, sinon c'est un double par défaut // 32bit
+	double pi = 3.141592653589793;// 64bit
+
+	return 0;
+
+
+}
--- TEMPLATE/td.c
+++ TEMPLATE/td.c
@@ -2,6 +2,6 @@
 
 int main(int argc, const char *argv[])
 {
-	puts("ahahahahahahahaahahaha je veux du fromage et du vin rouge et du pain aussi et du saucisson et du chocolat et du champagne");
+	puts("ahahahahahahahaahahaha je veux du fromage et chocolat et du champagne");
 	return 0;
 }
