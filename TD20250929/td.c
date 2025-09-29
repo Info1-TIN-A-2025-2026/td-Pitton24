4ad8abc21c73e38c0a79aabf40353a2f34879d20
Pitton24 <samuel.pitton@heig-vd.ch>
2025-09-29 12:29:18 +0000

    last version

---
+++ TD20250929/td.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	signed int s;
+	printf("Entrez le nombre de secondes : ");
+	scanf("%d", &s);
+	if (s < 0 || s >= 86400)
+	{
+		if (s < 0)
+			printf("Error.\n");
+		else
+			printf("Overflow\n");
+	}
+	else
+	{
+		printf("%02d : %02d : %02d \n", (s / 3600), (s % 3600) / 60, s % 60);
+	}
+	return 0;
+}
